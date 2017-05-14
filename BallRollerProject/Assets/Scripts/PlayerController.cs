442768fd4f0d802700d94caddfa2bde52f229560
Yi Li <yl2326@cornell.edu>
2017-05-14 16:07:37 -0700

    Init commit

---
+++ BallRollerProject/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+    public Text countText;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        countText.text = "Count: " + 0;
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        rb.AddForce(speed * new Vector3(moveHorizontal, 0.0f, moveVertical));
+        countText.text = "Movement is: " + moveHorizontal;
+    }
+}
